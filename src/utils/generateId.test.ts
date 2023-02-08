48d3fbeaa90ef45ac0ebfb02d0eb9388499c932c
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-02-08 22:50:13 +0100

    test: :test_tube: add generateId test

---
+++ src/utils/generateId.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from 'vitest';
+import generateId from './generateId';
+
+// Edit an assertion and save to see HMR in action
+
+test('generateId(4)', () => {
+  const generatedId = generateId(4);
+  expect(generatedId?.length).toBe(4);
+  expect(generatedId).toBeTypeOf('string');
+});
+
+test('generateId(0)', () => {
+  const generatedId = generateId(0);
+  expect(generatedId).toBeTypeOf('undefined');
+});
+
+test('generateId(-1)', () => {
+  const generatedId = generateId(-1);
+  expect(generatedId).toBeTypeOf('undefined');
+});
