61e851e886900773325424fe03d50218966b0bd0
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-02-14 16:33:30 +0100

    feat: :sparkles: add sort category

--- src/features/AddEditArticle/components/SelectCategory.tsx
+++ src/features/AddEditArticle/components/SelectCategory.tsx
@@ -8,6 +8,35 @@ interface Props {
   setFormState: React.Dispatch<React.SetStateAction<IFormState>>;
 }
 
+interface IOption {
+  label: string;
+  value: string;
+  children?: IOption[];
+}
+
+function createOptions(categories: SanityCategory[]) {
+  const roots = categories.filter((category) => !category.parent); // top-level categories
+
+  const createNode: any = (category: SanityCategory) => {
+    const children = categories.filter((c) => c.parent?._ref === category._id);
+    if (children.length === 0) {
+      const option = {
+        label: category.title,
+        value: category._id,
+      };
+      return option;
+    } else {
+      const option = {
+        label: category.title,
+        children: children.map((child) => createNode(child)),
+      };
+      return option;
+    }
+  };
+
+  return roots.map((root) => createNode(root));
+}
+
 const SelectCategory = ({ formState, categories, setFormState }: Props) => {
   const errorForm = useError(formState);
 
@@ -15,15 +44,27 @@ const SelectCategory = ({ formState, categories, setFormState }: Props) => {
     setFormState({ ...formState, articleCategory: event.target.value });
   }
 
+  const options: IOption[] = createOptions(categories);
+  const sortedOptions = sortOptionsByLabel(options);
+
   return (
     <p>
       <select value={formState?.articleCategory} onChange={handleChange}>
         <option value="0">Choisir une catégorie</option>
-        {categories.map((category) => (
-          <option key={category._id} value={category._id}>
-            {category.title}
-          </option>
-        ))}
+        {sortedOptions.map((option) => {
+          if (!option.children) {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            );
+          }
+          return option.children.map((subOption) => (
+            <option key={subOption.value} value={subOption.value}>
+              {option.label} - {subOption.label}
+            </option>
+          ));
+        })}
       </select>
       {errorForm?.errorCategory && <span>Veuillez saisir une catégorie</span>}
     </p>
@@ -31,3 +72,22 @@ const SelectCategory = ({ formState, categories, setFormState }: Props) => {
 };
 
 export default SelectCategory;
+
+function sortOptionsByLabel(array: IOption[]) {
+  array.sort(function (a, b) {
+    if (a.label < b.label) {
+      return -1;
+    }
+    if (a.label > b.label) {
+      return 1;
+    }
+    return 0;
+  });
+  for (var i = 0; i < array.length; i++) {
+    const children = array[i].children;
+    if (children) {
+      sortOptionsByLabel(children);
+    }
+  }
+  return array;
+}
