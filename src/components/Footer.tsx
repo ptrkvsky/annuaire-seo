aae142a9b1243586e341d8ff9d0cbdb43cacf519
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-02-17 23:13:14 +0100

    feat: :sparkles: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="conteneur">
+        <a href="https://helpukraine.center/">🇺🇦</a> - Mentions légales -
+        Politique de confidentialité
+      </div>
+    </footer>
+  );
+}
