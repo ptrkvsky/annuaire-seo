93c54f7c62a5ae0d8670509bbf73f68021456a8c
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-02-18 18:12:54 +0100

    feat: :sparkles: add listing articles

--- src/services/article/getArticlesByCategoryId.ts
+++ src/services/article/getArticlesByCategoryId.ts
@@ -3,7 +3,7 @@ import type { SanityArticle } from '@/interfaces/SanityArticle';
 import sanityClient from '@sanity/client';
 
 export default async function getArticlesByCategoryId(idCategory: string) {
-  const query = `*[_type == "article" && isActive == true && articleCategory._ref == $idCategory]`;
+  const query = `*[_type == "article" && isActive == true && articleCategory._ref == $idCategory]{title,isActive,content,slug,imageMain{asset->{path,url}}}`;
   const params = { idCategory };
   const client = sanityClient(sanityConfig);
   const articles = await client.fetch<SanityArticle[]>(query, params);
