7b1eab9e445124786f387ac176143eed0b61e8fb
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-02-22 09:19:06 +0100

    fix: :lock: update public keys

--- src/config/sanityConfig.ts
+++ src/config/sanityConfig.ts
@@ -1,8 +1,8 @@
 export const sanityConfig = {
-  projectId: import.meta.env.PUBLIC_SANITY_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  projectId: import.meta.env.SANITY_ID,
+  dataset: import.meta.env.SANITY_DATASET,
   apiVersion: '2021-08-31',
-  token: import.meta.env.PUBLIC_SANITY_TOKEN,
+  token: import.meta.env.SANITY_TOKEN,
   ignoreBrowserTokenWarning: true,
   useCdn: true,
 };
